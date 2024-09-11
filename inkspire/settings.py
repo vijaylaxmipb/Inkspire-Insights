75a8e89bcab269253567b59a7c0b3c1d5a0c98b6
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-09-11 13:16:18 +0000

    add database url to settings.py file

--- inkspire/settings.py
+++ inkspire/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-r+kgir4o4+(h!qol3s!z9-+oo8=#2%8jdhtj4t(_kar@l)h6u='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 
     '8000-vijaylaxmip-inkspireins-xcl7d7k6nab.ws.codeinstitute-ide.net',
@@ -77,12 +82,35 @@ WSGI_APPLICATION = 'inkspire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#DATABASES = {
+#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#}
+
+import dj_database_url
+import os
+
+# Get the DATABASE_URL environment variable
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise ValueError("The DATABASE_URL environment variable is not set")
+
+# Use dj_database_url to parse the database URL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL)
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
@@ -125,3 +153,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
