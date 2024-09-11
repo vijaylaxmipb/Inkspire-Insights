af2e999967785d55286ae52f3f9dcda33fa42351
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-09-11 22:39:34 +0000

    add a new comment model

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,16 @@ class Post(models.Model):
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
 )
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 content = models.TextField()
 created_on = models.DateTimeField(auto_now_add=True)
 status = models.IntegerField(choices=STATUS, default=0)
 excerpt = models.TextField(blank=True)
 updated_on = models.DateTimeField(auto_now=True)
 
-   
-    
